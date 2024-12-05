07dd5c1862d4a43389783c6c4a6b540748b3cc55
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-05 15:10:06 +0300

    update variable naming

--- backend/Routes/musicianRouter.js
+++ backend/Routes/musicianRouter.js
@@ -5,7 +5,7 @@ import { createNewMusicaian,getAdressFIlterdMusician, getCatagoryFIlterdMusician
 const router = express.Router();
 
 router.post("/",createNewMusicaian );
-router.get("/",getAdressFIlterdMusician );//filtered by adress
+router.get("/address",getAdressFIlterdMusician );//filtered by adress
 router.get("/catagory",getCatagoryFIlterdMusician );//filtered by catagory
 router.get("/both",getCategoryAndAddressFilteredMusician );//filtered by catagory & adress
 router.get("/",getAllMusicians );
