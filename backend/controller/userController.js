09a9f189c1e483b7aceb7f10442662688c52c232
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-17 15:21:18 +0300

    setup all the router and controller of login and sign up

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,12 +1,79 @@
-import User from "../model/user.model.js";
+import User from "../model/user.model.js"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import validator from 'validator'
 
-const signUp= async (req,res)=>{
+const createToken = (_id)=>{
+  return jwt.sign({_id},process.env.JWTSECRATEKEY , { expiresIn : "10d"} )
+}
+
+const register= async (req,res)=>{
+  try {
+    const { firstName, lastName , email , password } =req.body
+    //all field validation
+    if (!(firstName && lastName && email && password)) {
+      return res.status(400).json("All fields are required")
+    }
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("Invalid email format")
+    }
+    
+
+
+    //already exist validator 
+    const emailAlreadyExists = await User.findOne({email})
+    if (emailAlreadyExists) {
+      return res.status(400).json("User Already exists with this Email")
+    }
+    //encrypt pass
+    const encPassword = await bcrypt.hash(password, 10)
+    //save db
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: encPassword
+    })
+    
+    const token = createToken(user._id)
+
+    res.status(200).json( {firstName , token })
+
+  } catch (error) {
+    console.log(error)
+  }
   
 }
 
 
 const logIn= async (req,res)=>{
+  try {
 
+    const { email , password } =req.body
+    //all field validation
+    if (!(email && password)) {
+      return res.status(400).json("All fields are required")
+    }
+
+    const user = await User.findOne({email})
+    if (!user) {
+      return res.status(400).json("Invalid Email")
+    }
+    //match pass
+    const much = await bcrypt.compare(password , user.password )
+    if (!much) {
+      return res.status(200).json("Incorrect password")
+    }
+
+    //send token
+    if ( user && much ) {
+      const token = createToken(user._id)
+      return res.status(200).json({token})
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-export {signUp,logIn}
+export { register , logIn }
