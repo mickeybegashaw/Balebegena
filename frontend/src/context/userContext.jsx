e16dc0419fca4b3ace8d4e5017c0d0fe02d29694
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-20 15:57:54 +0300

    make the user state update always the component render

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,4 +1,4 @@
-import { createContext , useState } from "react";
+import { createContext , useState, useEffect } from "react";
 
 export const AuthContext = createContext() 
 
@@ -6,6 +6,12 @@ const UserContextProvider = ({children}) => {
   const [ user , setUser ] =useState(null)
   const [ error , setError ] =useState(null)
   const [ loading , setLoading ] =useState(false)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem('user'))
+    if (user) {
+      setUser(user)
+    }
+  },[])
 
   return (
     <AuthContext.Provider value={{user , setUser ,error , setError,loading , setLoading }}>
